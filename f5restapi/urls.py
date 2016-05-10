69df4fab2b296504020354bc0fe3b3ffbc27fecb
Your Name <you@example.com>
2016-05-10 14:48:37 +0900

    update

--- f5restapi/urls.py
+++ f5restapi/urls.py
@@ -3,9 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from f5restapi import views
 
 urlpatterns = [
-    url(r'^f5/devicelist/', views.f5_devicelist),
-    url(r'^f5/virtualserverlist/', views.f5_virtualserverlist),
-    url(r'^f5/poolmemberlist/', views.f5_poolmemberlist),
+    url(r'^f5/devicelist/$', views.f5_devicelist),
+    url(r'^f5/virtualserverlist/$', views.f5_virtualserverlist),
+    url(r'^f5/poolmemberlist/$', views.f5_poolmemberlist),
+    url(r'^f5/poolmemberlist/(?P<poolname>[0-9A-Za-z_.-]+)/$', views.f5_poolmemberstatus),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
