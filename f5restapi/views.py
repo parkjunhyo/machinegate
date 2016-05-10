d929a593dc39ddf2ea6ddcd31f195aa0f50521be
Your Name <you@example.com>
2016-05-10 14:49:12 +0900

    update

--- f5restapi/views.py
+++ f5restapi/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from f5restapi.f5views.f5_devicelist import f5_devicelist 
 from f5restapi.f5views.f5_virtualserverlist import f5_virtualserverlist
 from f5restapi.f5views.f5_poolmemberlist import f5_poolmemberlist
+from f5restapi.f5views.f5_poolmemberstatus import f5_poolmemberstatus
 
