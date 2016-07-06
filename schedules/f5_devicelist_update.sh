dd6846b98f87fdbc9503214507737a1231da25cf
Your Name <you@example.com>
2016-07-06 11:45:46 +0900

    update

---
+++ schedules/f5_devicelist_update.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+F5RESTAPI_PATH=$(find / -name 'f5restapi')
+
+SETTING_DIR=$F5RESTAPI_PATH/setting.py
+
+ENCAP_PASSWORD=$(cat $SETTING_DIR | grep -i 'ENCAP_PASSWORD = ' | awk -F "[\"]" '{print $2}')
+RUNSERVER_PORT=$(cat $SETTING_DIR | grep -i 'RUNSERVER_PORT = ' | awk -F "[']" '{print $2}')
+
+curl -H "Accept: application/json" -X POST -d "[{\"auth_key\":\"$ENCAP_PASSWORD\"}]" http://localhost:$RUNSERVER_PORT/f5/devicelist/ > /dev/null
