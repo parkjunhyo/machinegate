9d34edcb81a7bc9bb063a9f92428cf1ce4ce9259
park junhyo <junhyo.luck@gmail.com>
2016-09-22 03:12:23 +0900

    Update setting.py

--- f5restapi/setting.py
+++ f5restapi/setting.py
@@ -15,12 +15,15 @@ LOG_FILE = USER_F5_DIR + "access.log"
 
 THREAD_TIMEOUT = 1
 
+# define the number for the stats rest api return number, 900 means 3day if you set up 5 minutes
 STATS_VIEWER_COUNT = 900
 
+# define the number how many days you can save, 31 means 1 month
 STATS_SAVEDDATA_MULTI = 31
 
 STATS_TOP_COUNT = 10
 
 RUNSERVER_PORT = '8080'
 
+# define the number how many days you rollback 288 mean, 1day
 ROLLBAK_INTERVAL = 288
