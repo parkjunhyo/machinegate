73d9c64916c14e79680cad580ec845a3f977b55c
Your Name <you@example.com>
2016-12-21 18:12:25 +0900

    update

--- juniperapi/juniperviews/juniper_exportpolicy.py
+++ juniperapi/juniperviews/juniper_exportpolicy.py
@@ -16,9 +16,11 @@ from juniperapi.setting import RUNSERVER_PORT
 from juniperapi.setting import PARAMIKO_DEFAULT_TIMEWAIT
 from juniperapi.setting import USER_VAR_POLICIES
 from juniperapi.setting import POLICY_FILE_MAX
+from juniperapi.setting import PYTHON_MULTI_PROCESS
 
 import os,re,copy,json,time,threading,sys
 import paramiko
+from multiprocessing import Process, Queue, Lock
 
 class JSONResponse(HttpResponse):
     """
@@ -46,6 +48,7 @@ def start_end_parse_from_string(return_lines_string,pattern_start,pattern_end):
 
 def run_command(_command_,fromtozone_pair,_ipaddress_,_hostname_):
 
+   print "%(_hostname_)s, %(_command_)s ... start" % {"_command_":str(_command_),"_hostname_":str(_hostname_)}
    # find the all counter
    pattern_string = "show security policies from-zone ([a-zA-Z0-9\-\_\.]+) to-zone ([a-zA-Z0-9\-\_\.]+) count ([a-zA-Z0-9\-\_\.]+) start ([a-zA-Z0-9\-\_\.]+) detail | no-more\n"
    searched_value = re.search(pattern_string,_command_,re.I)
@@ -53,14 +56,14 @@ def run_command(_command_,fromtozone_pair,_ipaddress_,_hostname_):
    _to_zone_ = searched_value.group(2)
    _start_count_ = searched_value.group(4)
    
-   max_count = POLICY_FILE_MAX
-   for _dictvalue_ in fromtozone_pair:
-      if re.match(str(_from_zone_),str(_dictvalue_['from']),re.I) and re.match(str(_to_zone_),str(_dictvalue_['to']),re.I):
-        max_count = _dictvalue_['count']
-        break
+   #max_count = POLICY_FILE_MAX
+   #for _dictvalue_ in fromtozone_pair:
+   #   if re.match(str(_from_zone_),str(_dictvalue_['from']),re.I) and re.match(str(_to_zone_),str(_dictvalue_['to']),re.I):
+   #     max_count = _dictvalue_['count']
+   #     break
 
-   # time parameter 60s * 4 min
-   hold_timeout = 60 * 5
+   # time parameter 180 seconds
+   hold_timeout = 180
    # connect              
    remote_conn_pre = paramiko.SSHClient()
    remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -72,111 +75,116 @@ def run_command(_command_,fromtozone_pair,_ipaddress_,_hostname_):
    output = remote_conn.recv(2097152)
    remote_conn_pre.close()
 
+   print "%(_hostname_)s, %(_command_)s ... obtain from device.." % {"_command_":str(_command_),"_hostname_":str(_hostname_)}
    # file write
    filename_string = "%(_hostname_)s@%(_ipaddress_)s_from_%(_from_zone_)s_to_%(_to_zone_)s_start_%(_start_count_)s.policy" % {"_ipaddress_":str(_ipaddress_),"_hostname_":str(_hostname_),"_start_count_":str(_start_count_),"_from_zone_":str(_from_zone_),"_to_zone_":str(_to_zone_)} 
    JUNIPER_DEVICELIST_DBFILE = USER_VAR_POLICIES + filename_string
    f = open(JUNIPER_DEVICELIST_DBFILE,"w")
    f.write(output)
    f.close()
-   # thread timeout 
-   time.sleep(1) 
+   print "%(_hostname_)s, %(_command_)s ... done/completed." % {"_command_":str(_command_),"_hostname_":str(_hostname_)}
+   #
+   time.sleep(0)
 
- 
 
-def export_policy(_ipaddress_,_hostname_):
-
-   # connect 
-   remote_conn_pre = paramiko.SSHClient()
-   remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-   remote_conn_pre.connect(_ipaddress_, username=USER_NAME, password=USER_PASSWORD, look_for_keys=False, allow_agent=False)
-   remote_conn = remote_conn_pre.invoke_shell()
-   remote_conn.send("show security policies zone-context | no-more\n")
-   remote_conn.send("exit\n")
-   time.sleep(PARAMIKO_DEFAULT_TIMEWAIT)
-   output = remote_conn.recv(20000)
-   remote_conn_pre.close() 
-
-   return_lines_string = output.split("\r\n")
-   pattern_start = "From zone[ \t\n\r\f\v]+To zone[ \t\n\r\f\v]+Policy count+"
-   pattern_end = "%(_hostname_)s> exit" % {"_hostname_":_hostname_}
-   start_end_linenumber_list = start_end_parse_from_string(return_lines_string,pattern_start,pattern_end)
- 
-   fromtozone_pair = []
-   for _indexlist_ in start_end_linenumber_list:
-      string_line = return_lines_string[_indexlist_[0]+1:_indexlist_[-1]-1]
-      for _line_ in string_line:
-         list_line = _line_.strip().split()
-         dictBox = {}
-         if len(list_line) == 3:
-           dictBox["from"] = list_line[0]
-           dictBox["to"] = list_line[1]
-           dictBox["count"] = list_line[2]
-           fromtozone_pair.append(dictBox)
-
-   # create command : POLICY_FILE_MAX, show security policies from-zone PRI to-zone COM count 100 start 100 | no-more
-   export_command_list = [] 
-   for _dictvalue_ in fromtozone_pair:
-      # parameter
-      _policy_count_ = int(_dictvalue_["count"])
-      div_count = int(_policy_count_/POLICY_FILE_MAX)
-      _from_zone_ = str(_dictvalue_["from"])
-      _to_zone_ = str(_dictvalue_["to"])
-      # create command
-      if div_count <= int(0):
-        _command_ = "show security policies from-zone %(_from_)s to-zone %(_to_)s count %(_maxcount_)s start %(_start_)s detail | no-more\n" % {"_from_":_from_zone_,"_to_":_to_zone_,"_maxcount_":str(_policy_count_),"_start_":str(1)}
-        export_command_list.append(_command_)
-      else:
-        _mod_value_ = int(_policy_count_ % POLICY_FILE_MAX)
-        if _mod_value_ == int(0):
-          list_range = range(div_count)
-        else:
-          list_range = range(div_count+1)
-        for _i_ in list_range:
-           start_value = int((POLICY_FILE_MAX * _i_) + 1)
-           _command_ = "show security policies from-zone %(_from_)s to-zone %(_to_)s count %(_maxcount_)s start %(_start_)s detail | no-more\n" % {"_from_":_from_zone_,"_to_":_to_zone_,"_maxcount_":str(POLICY_FILE_MAX),"_start_":str(start_value)}
-           export_command_list.append(_command_)
-
-   # depend on the device performace you can adjust this number below : SRX 1400 : MAX 3     
-   multi_access_ssh_usernumber = int(3)     
-   ( _divnumber_, _modnumber_ ) = divmod(len(export_command_list),multi_access_ssh_usernumber)
-   _loopinglist_ = []
-   if int(_modnumber_) == int(0):
-     _looptotalcount_ = int(_divnumber_)
+def regroup_by_number(origin_list, divinterger):
+   # 
+   processing_combination = []
+   if len(origin_list) <= int(divinterger):
+     for _i_ in range(len(origin_list)):
+        processing_combination.append([])
    else:
-     _looptotalcount_ = int(_divnumber_) + 1
-   _loopinglist_ = range(_looptotalcount_)
-    
-   _loopcount_ = 1
-   for _loopid_ in _loopinglist_:
-      _splited_export_command_list_ = [] 
-      _start_id_ = int(_loopid_) * int(multi_access_ssh_usernumber)
-      _end_id_ = ( int(_loopid_) + 1 ) * int(multi_access_ssh_usernumber)
-      if int(_loopcount_) != int(_looptotalcount_):
-        _splited_export_command_list_ = export_command_list[_start_id_:_end_id_]
+     for _i_ in range(int(divinterger)):
+        processing_combination.append([])
+   # 
+   count = 0
+   for parameter_combination in origin_list:
+      (_values_, _last_) = divmod(count, int(int(divinterger)))
+      processing_combination[_last_].append(parameter_combination)
+      count = count + 1
+   return processing_combination
+
+
+def export_policy(_each_processorData_, ip_device_dict):
+   #
+   for _ipaddress_ in _each_processorData_:
+
+      _hostname_ = ip_device_dict[_ipaddress_]
+
+      # connect 
+      remote_conn_pre = paramiko.SSHClient()
+      remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      remote_conn_pre.connect(_ipaddress_, username=USER_NAME, password=USER_PASSWORD, look_for_keys=False, allow_agent=False)
+      remote_conn = remote_conn_pre.invoke_shell()
+      remote_conn.send("show security policies zone-context | no-more\n")
+      remote_conn.send("exit\n")
+      time.sleep(PARAMIKO_DEFAULT_TIMEWAIT)
+      output = remote_conn.recv(20000)
+      remote_conn_pre.close() 
+
+      return_lines_string = output.split("\r\n")
+      pattern_start = "From zone[ \t\n\r\f\v]+To zone[ \t\n\r\f\v]+Policy count+"
+      pattern_end = "%(_hostname_)s> exit" % {"_hostname_":_hostname_}
+      start_end_linenumber_list = start_end_parse_from_string(return_lines_string,pattern_start,pattern_end)
+ 
+      fromtozone_pair = []
+      for _indexlist_ in start_end_linenumber_list:
+         string_line = return_lines_string[_indexlist_[0]+1:_indexlist_[-1]-1]
+         for _line_ in string_line:
+            list_line = _line_.strip().split()
+            dictBox = {}
+            if len(list_line) == 3:
+              dictBox["from"] = list_line[0]
+              dictBox["to"] = list_line[1]
+              dictBox["count"] = list_line[2]
+              fromtozone_pair.append(dictBox)
+
+      # create command : POLICY_FILE_MAX, show security policies from-zone PRI to-zone COM count 100 start 100 | no-more
+      export_command_list = [] 
+      for _dictvalue_ in fromtozone_pair:
+         # parameter
+         _policy_count_ = int(_dictvalue_["count"])
+         div_count = int(_policy_count_/POLICY_FILE_MAX)
+         _from_zone_ = str(_dictvalue_["from"])
+         _to_zone_ = str(_dictvalue_["to"])
+         # create command
+         if div_count <= int(0):
+           _command_ = "show security policies from-zone %(_from_)s to-zone %(_to_)s count %(_maxcount_)s start %(_start_)s detail | no-more\n" % {"_from_":_from_zone_,"_to_":_to_zone_,"_maxcount_":str(_policy_count_),"_start_":str(1)}
+           export_command_list.append(_command_)
+         else:
+           _mod_value_ = int(_policy_count_ % POLICY_FILE_MAX)
+           if _mod_value_ == int(0):
+             list_range = range(div_count)
+           else:
+             list_range = range(div_count+1)
+           for _i_ in list_range:
+              start_value = int((POLICY_FILE_MAX * _i_) + 1)
+              _command_ = "show security policies from-zone %(_from_)s to-zone %(_to_)s count %(_maxcount_)s start %(_start_)s detail | no-more\n" % {"_from_":_from_zone_,"_to_":_to_zone_,"_maxcount_":str(POLICY_FILE_MAX),"_start_":str(start_value)}
+              export_command_list.append(_command_)
+
+
+      # depend on the device performace you can adjust this number below : SRX 1400 : MAX 3     
+      multi_access_ssh_usernumber = int(3)     
+      (_values_, _last_) = divmod(len(export_command_list),multi_access_ssh_usernumber)
+      login_number = 0
+      if int(_last_) == int(0):
+        login_number = int(_values_)
       else:
-        _splited_export_command_list_ = export_command_list[_start_id_:]        
-      _loopcount_ = _loopcount_ + 1 
-    
-      _threads_ = []
-      for _command_ in _splited_export_command_list_:      
-         th = threading.Thread(target=run_command, args=(_command_,fromtozone_pair,_ipaddress_,_hostname_,))
-         th.start()
-         _threads_.append(th)
-      for th in _threads_:
-         th.join()   
+        login_number = int(_values_) + 1
+      _splited_export_command_list_ = regroup_by_number(export_command_list, login_number)
 
-
-
-   #_threads_ = []
-   #for _command_ in export_command_list:      
-   #   th = threading.Thread(target=run_command, args=(_command_,fromtozone_pair,_ipaddress_,_hostname_,))
-   #   th.start()
-   #   _threads_.append(th)
-   #for th in _threads_:
-   #   th.join()   
+      for _command_group_ in _splited_export_command_list_:
+         threadlist = []
+         for _command_ in _command_group_:
+            _thread_ = threading.Thread(target=run_command, args=(_command_,fromtozone_pair,_ipaddress_,_hostname_,))
+            _thread_.start()
+            threadlist.append(_thread_)
+         for _thread_ in threadlist:
+            _thread_.join()
+         
 
    # thread timeout 
-   time.sleep(10)
+   time.sleep(0)
 
 def viewer_information():
    filenames_list = os.listdir(USER_VAR_POLICIES)
@@ -246,13 +254,19 @@ def juniper_exportpolicy(request,format=None):
                     ip_device_dict[_ipaddress_] = str(dataDict_value[u"devicehostname"])
                     valid_access_ip.append(_ipaddress_)
 
-           _threads_ = []
-           for _ipaddress_ in valid_access_ip:
-              th = threading.Thread(target=export_policy, args=(_ipaddress_,ip_device_dict[_ipaddress_],))
-              th.start()
-              _threads_.append(th)
-           for th in _threads_:
-              th.join()
+           # init
+           processing_combination = []
+           processing_combination = regroup_by_number(valid_access_ip, PYTHON_MULTI_PROCESS)
+           # 
+           count = 0
+           _processor_list_ = []
+           for _each_processorData_ in processing_combination:
+              _processor_ = Process(target = export_policy, args = (_each_processorData_, ip_device_dict,))
+              _processor_.start()
+              _processor_list_.append(_processor_)
+           for _processor_ in _processor_list_:
+              _processor_.join()
+           print "export processors are completed..!"
 
            # return
            return Response(viewer_information())
