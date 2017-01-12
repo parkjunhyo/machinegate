932bc3f820953db3b84154906c8c4808c98eed5a
Your Name <you@example.com>
2017-01-12 19:11:03 +0900

    update

--- juniperapi/juniperviews/juniper_cachingnat.py
+++ juniperapi/juniperviews/juniper_cachingnat.py
@@ -21,6 +21,7 @@ from juniperapi.setting import USER_VAR_CHCHES
 import os,re,copy,json,time,threading,sys
 import paramiko
 from multiprocessing import Process, Queue, Lock
+from netaddr import *
 
 class JSONResponse(HttpResponse):
     """
@@ -62,45 +63,25 @@ def start_end_parse_from_string_endlist(return_lines_string,pattern_start,patter
       line_index_count = line_index_count + 1
    return start_end_linenumber_list
 
-def findout_policyname_sequence(everypolicy_group):
-   # default init values
-   _policyname_status_ = False
-   _policysequence_status_ = False
-   _policyzone_status_ = False  
-   # common pattern :([a-zA-Z0-9,_\-:;\.]+)
-   policyname_spattern = r"Policy: ([a-zA-Z0-9_\-:;\.]+)[,] action-type: ([a-zA-Z0-9_\-:;\.]+)"
-   policysequence_spattern = r"Sequence number: ([0-9]+)"
-   policyzone_spattern = r"From zone: ([a-zA-Z0-9_\-:;\.]+)[,] To zone: ([a-zA-Z0-9_\-:;\.]+)"
-   # init values
-   searched_policyname = "none"
-   _policyaction_status_ = "deny"
-   searched_sequencenumber = "none"
-   searched_from = "none"
-   searched_to = "none"
-   # processing
-   for _string_line_ in everypolicy_group:
-      if not (_policyname_status_ and _policysequence_status_ and _policyzone_status_):
-        # policy name and status
-        searching_content = re.search(str(policyname_spattern), str(_string_line_), re.I)
-        if searching_content:
-          _policyname_status_ = True
-          searched_policyname = searching_content.group(1)
-          _policyaction_status_ = searching_content.group(2)
-        # sequece number 
-        searching_content = re.search(policysequence_spattern, str(_string_line_), re.I)
-        if searching_content:
-          _policysequence_status_ = True   
-          searched_sequencenumber = searching_content.group(1) 
-        # from to zone name  
-        searching_content = re.search(policyzone_spattern, str(_string_line_), re.I) 
-        if searching_content:
-          _policyzone_status_ = True
-          searched_from = searching_content.group(1)
-          searched_to = searching_content.group(2)
-   # return
-   return (searched_policyname, _policyaction_status_, searched_sequencenumber, searched_from, searched_to)
 
+def searched_ip_pattern(inner_msg_string_, searched_ip_list):
+   searched_portstatus = re.search("([0-9]+.[0-9]+.[0-9]+.[0-9]+)[ \t\n\r\f\v]+\-[ \t\n\r\f\v]+([0-9]+.[0-9]+.[0-9]+.[0-9]+)", inner_msg_string_, re.I)
+   if searched_portstatus:
+     ip_list = list(iter_iprange(str(searched_portstatus.group(1)), str(searched_portstatus.group(2))))
+     for _netip_ in ip_list:
+        _netip_string_ = str(_netip_).strip()
+        if _netip_string_ not in searched_ip_list:
+          searched_ip_list.append(_netip_string_)   
+   return searched_ip_list
 
+def findout_iplist_from_range(selective_list, inner_pattern_start, inner_pattern_end):
+   searched_ip_list = []
+   inner_start_end_linenumber_list = start_end_parse_from_string(selective_list, inner_pattern_start, inner_pattern_end)
+   for inner_index_list in inner_start_end_linenumber_list:
+      inner_selective_list = selective_list[inner_index_list[0]:inner_index_list[-1]]
+      for inner_msg_string_ in inner_selective_list:
+         searched_ip_list = searched_ip_pattern(inner_msg_string_, searched_ip_list)
+   return searched_ip_list
 
 def cachingnat_processing(_accessip_, _hostname_):
    #
@@ -113,89 +94,165 @@ def cachingnat_processing(_accessip_, _hostname_):
         if searched_filename not in valied_filename:
           valied_filename.append(searched_filename)
 
-   for _filename_ in valied_filename:
-      if re.search(".nat.static.rule", _filename_, re.I):
 
-        pattern_start = "Static NAT rule:"
-        pattern_end = "Number of sessions[ \t\n\r\f\v]+:"
-      #
+   for _filename_ in valied_filename:
+
+      if re.search(".nat.static.rule", _filename_, re.I):
+        natcache_memory = {}
+        #
         f = open(_filename_, "r")
         read_contents = f.readlines()
         f.close()
+        #
+        pattern_start = "Static NAT rule:"
+        pattern_end = "Number of sessions[ \t\n\r\f\v]+:"
         start_end_linenumber_list = start_end_parse_from_string(read_contents, pattern_start, pattern_end)
-
-        static_cache_dict = {}
         for index_list in start_end_linenumber_list:
+           # range to items
            selective_list = read_contents[index_list[0]:index_list[-1]+int(1)]
-
-           #
+           # default init
            _natrulename_ = "unknown"
            _natrulesetname_ = "unknown"
            _fromzonename_ = "unknown"
            _destaddress_ = "unknown"
            _hostaddress_ = "unknown"
            _netmask_ = "unknown"
-           #
-           
+           # analysis each line of the one items
            for _msg_string_ in selective_list:
               compare_string = _msg_string_.strip()
               splited_compare_string = compare_string.split()
-
               if re.search("Static NAT rule:", compare_string, re.I):
                 _natrulename_ = splited_compare_string[3] 
                 _natrulesetname_ = splited_compare_string[-1]
-
               if re.search("From zone", compare_string, re.I):
                 _fromzonename_ = splited_compare_string[-1]
-
               if re.search("Destination addresses", compare_string, re.I):
                 _destaddress_ = splited_compare_string[-1]
-
               if re.search("Host addresses", compare_string, re.I):
                 _hostaddress_ = splited_compare_string[-1]
-
               if re.search("Netmask", compare_string, re.I):
                 _netmask_ = splited_compare_string[-1]
+           # change to values 
+           _ruleunique_ = "%(_natrulename_)s:%(_natrulesetname_)s" % {"_natrulename_":_natrulename_, "_natrulesetname_":_natrulesetname_}
+           _destaddress_string_ = "%(_destaddress_)s/%(_netmask_)s@%(_hostname_)s:static_from_%(_fromzonename_)s@%(_ruleunique_)s" % {"_destaddress_":_destaddress_, "_hostname_":_hostname_, "_fromzonename_":_fromzonename_, "_netmask_":_netmask_, "_ruleunique_":_ruleunique_}
+           _hostaddress_string_ = "%(_hostaddress_)s/%(_netmask_)s@%(_hostname_)s:static_from_%(_fromzonename_)s@%(_ruleunique_)s" % {"_hostaddress_":_hostaddress_, "_hostname_":_hostname_, "_fromzonename_":_fromzonename_, "_netmask_":_netmask_, "_ruleunique_":_ruleunique_}
+           # 
+           keyname_string = "%(_ipaddrvalue_)s/%(_netmask_)s" % {"_netmask_":_netmask_,"_ipaddrvalue_":_destaddress_}
+           if keyname_string  not in natcache_memory.keys():
+             natcache_memory[keyname_string] = []
+           natcache_memory[keyname_string].append(_hostaddress_string_)
 
+           keyname_string = "%(_ipaddrvalue_)s/%(_netmask_)s" % {"_netmask_":_netmask_,"_ipaddrvalue_":_hostaddress_}
+           if keyname_string not in natcache_memory.keys():
+             natcache_memory[keyname_string] = []           
+           natcache_memory[keyname_string].append(_destaddress_string_)
+        #
+        saved_filename = _filename_.strip().split("/")[-1]
+        filename_string = "cachenat_%(saved_filename)s.txt" % {"saved_filename":str(saved_filename)}
+        JUNIPER_DEVICELIST_DBFILE = USER_VAR_CHCHES + filename_string
+        f = open(JUNIPER_DEVICELIST_DBFILE,"w")
+        f.write(json.dumps(natcache_memory))
+        f.close() 
+
+      if re.search(".nat.source.rule", _filename_, re.I):
+        natcache_memory = {}
+        #
+        sourcepool_filename = _filename_.strip().split(".nat.source.rule")[0] + ".nat.source.pool"
+        f = open(sourcepool_filename, "r")
+        read_contents = f.readlines()
+        f.close()
+        #
+        pattern_start = "Pool name[ \t\n\r\f\v]+:"
+        pattern_end = "Address range"
+        start_end_linenumber_list = start_end_parse_from_string(read_contents, pattern_start, pattern_end)
+        sourcepool_memory = {}
+        for index_list in start_end_linenumber_list:
+           # range to items
+           selective_list = read_contents[index_list[0]:index_list[-1]+int(2)]
+           # default init
+           _poolname_ = "unknown"
+           _sourceport_ = "unknown"
+           _sourceip_ = []
+           for _msg_string_ in selective_list:
+              compare_string = _msg_string_.strip()
+              if re.search("Pool name[ \t\n\r\f\v]+:", compare_string, re.I):
+                _poolname_ = compare_string.split()[-1]
+              searched_portstatus = re.search("^Port[ \t\n\r\f\v]+:[ \t\n\r\f\v]+\[([0-9]+),[ \t\n\r\f\v]+([0-9]+)\]", compare_string, re.I)
+              if searched_portstatus:
+                _sourceport_ = "%(_start_)s:%(_finish_)s" % {"_start_":str(searched_portstatus.group(1)), "_finish_":str(searched_portstatus.group(2))} 
+              _sourceip_ = searched_ip_pattern(compare_string, _sourceip_) 
            #  
-           _destaddress_string_ = "%(_destaddress_)s/%(_netmask_)s@%(_hostname_)s:static_from_%(_fromzonename_)s" % {"_destaddress_":_destaddress_, "_hostname_":_hostname_, "_fromzonename_":_fromzonename_, "_netmask_":_netmask_}
-           _hostaddress_string_ = "%(_hostaddress_)s/%(_netmask_)s@%(_hostname_)s:static_from_%(_fromzonename_)s" % {"_hostaddress_":_hostaddress_, "_hostname_":_hostname_, "_fromzonename_":_fromzonename_, "_netmask_":_netmask_}
+           if _poolname_ not in sourcepool_memory.keys():
+             sourcepool_memory[_poolname_] = []
+           for _ip_ in _sourceip_:
+              member_string = "%(_ip_)s_%(_sourceport_)s" % {"_ip_":_ip_, "_sourceport_":_sourceport_}
+              if member_string not in sourcepool_memory[_poolname_]:
+                sourcepool_memory[_poolname_].append(member_string)
 
-           #
-           _keyname_ = "%(_destaddress_)s/%(_netmask_)s" % {"_destaddress_":_destaddress_, "_netmask_":_netmask_}
-           if _keyname_ not in static_cache_dict.keys():
-             static_cache_dict[_keyname_] = {}
-             static_cache_dict[_keyname_][u"rulename"] = []
-             static_cache_dict[_keyname_][u"rulesetname"] = []
-             static_cache_dict[_keyname_][u"changeto"] = []
+        #_filename_
+        f = open(_filename_, "r")
+        read_contents = f.readlines()
+        f.close()
+        #
+        pattern_start = "source NAT rule:"
+        pattern_end = "Number of sessions[ \t\n\r\f\v]+:"
+        start_end_linenumber_list = start_end_parse_from_string(read_contents, pattern_start, pattern_end)
 
-           if _natrulename_ not in static_cache_dict[_keyname_][u"rulename"]:
-             static_cache_dict[_keyname_][u"rulename"].append(_natrulename_)
- 
-           if _natrulesetname_ not in static_cache_dict[_keyname_][u"rulesetname"]:
-             static_cache_dict[_keyname_][u"rulesetname"].append(_natrulesetname_)
+        for index_list in start_end_linenumber_list:
+           # range to items
+           selective_list = read_contents[index_list[0]:index_list[-1]+int(1)]
+           # source and destination address
+           _sourceipvaluelist_ = findout_iplist_from_range(selective_list, "Source addresses[ \t\n\r\f\v]+:", "Destination addresses[ \t\n\r\f\v]+:")
+           # init value
+           _natrulename_ = "unknown"
+           _natrulesetname_ = "unknown"
+           _fromzonename_ = "unknown"
+           _tozonename_ = "unknown"
+           _action_ = "unknown"
+           # analysis each line of the one items
+           for _msg_string_ in selective_list:
+              compare_string = _msg_string_.strip()
+              splited_compare_string = compare_string.split()
+              if re.search("source NAT rule:", compare_string, re.I):
+                _natrulename_ = splited_compare_string[3]
+                _natrulesetname_ = splited_compare_string[-1]
+              if re.search("From zone[ \t\n\r\f\v]+:", compare_string, re.I):
+                _fromzonename_ = splited_compare_string[-1]
+              if re.search("To zone[ \t\n\r\f\v]+:", compare_string, re.I):
+                _tozonename_ = splited_compare_string[-1]
+              if re.search("Action[ \t\n\r\f\v]+:", compare_string, re.I):
+                _action_ = splited_compare_string[-1]
+           # 
+           _ruleunique_ = "%(_natrulename_)s:%(_natrulesetname_)s" % {"_natrulename_":_natrulename_, "_natrulesetname_":_natrulesetname_} 
+           for _srcip_ in _sourceipvaluelist_:
+              _srcip_string_ = "%(_srcip_)s/32" % {"_srcip_":_srcip_}
+              if _srcip_string_ not in natcache_memory.keys():
+                natcache_memory[_srcip_string_] = []
+              for _targetip_ in sourcepool_memory[_action_]:
+                 _targetip_value_ = str(_targetip_.strip().split("_")[0])
+                 _targetip_string_ = "%(_targetip_value_)s/32@%(_hostname_)s:source_from_%(_fromzonename_)s_to_%(_tozonename_)s@%(_ruleunique_)s" % {"_targetip_value_":_targetip_value_, "_hostname_":_hostname_, "_fromzonename_":_fromzonename_, "_tozonename_":_tozonename_, "_ruleunique_":_ruleunique_}
+                 if _targetip_string_ not in natcache_memory[_srcip_string_]:
+                   natcache_memory[_srcip_string_].append(_targetip_string_) 
+           # 
+           for _targetip_ in sourcepool_memory[_action_]:
+              _targetip_value_ = str(_targetip_.strip().split("_")[0])
+              _targetip_string_ = "%(_targetip_value_)s/32" % {"_targetip_value_":_srcip_}
+              if _targetip_string_ not in natcache_memory.keys():
+                natcache_memory[_targetip_string_] = []
+              for _srcip_ in _sourceipvaluelist_:
+                 _srcip_string_ = "%(_srcip_)s/32@%(_hostname_)s:source_from_%(_fromzonename_)s_to_%(_tozonename_)s@%(_ruleunique_)s" % {"_srcip_":_srcip_, "_hostname_":_hostname_, "_fromzonename_":_fromzonename_, "_tozonename_":_tozonename_, "_ruleunique_":_ruleunique_}
+                 if _srcip_string_ not in natcache_memory[_targetip_string_]:
+                   natcache_memory[_targetip_string_].append(_srcip_string_)
 
-           if _hostaddress_string_ not in static_cache_dict[_keyname_][u"changeto"]:
-             static_cache_dict[_keyname_][u"changeto"].append(_hostaddress_string_)
+        #
+        saved_filename = _filename_.strip().split("/")[-1]
+        filename_string = "cachenat_%(saved_filename)s.txt" % {"saved_filename":str(saved_filename)}
+        JUNIPER_DEVICELIST_DBFILE = USER_VAR_CHCHES + filename_string
+        f = open(JUNIPER_DEVICELIST_DBFILE,"w")
+        f.write(json.dumps(natcache_memory))
+        f.close()
+        #
 
-
-           _keyname_ = "%(_hostaddress_)s/%(_netmask_)s" % {"_hostaddress_":_hostaddress_, "_netmask_":_netmask_}
-           if _keyname_ not in static_cache_dict.keys():
-             static_cache_dict[_keyname_] = {}
-             static_cache_dict[_keyname_][u"rulename"] = []
-             static_cache_dict[_keyname_][u"rulesetname"] = []
-             static_cache_dict[_keyname_][u"changeto"] = []
-
-           if _natrulename_ not in static_cache_dict[_keyname_][u"rulename"]:
-             static_cache_dict[_keyname_][u"rulename"].append(_natrulename_)
-
-           if _natrulesetname_ not in static_cache_dict[_keyname_][u"rulesetname"]:
-             static_cache_dict[_keyname_][u"rulesetname"].append(_natrulesetname_)
-
-           if _destaddress_string_ not in static_cache_dict[_keyname_][u"changeto"]:
-             static_cache_dict[_keyname_][u"changeto"].append(_destaddress_string_)
-
-        print static_cache_dict
 
    # thread timeout 
    time.sleep(1)
@@ -279,13 +336,6 @@ def juniper_cachingnat(request,format=None):
            for _processor_ in _processor_list_:
               _processor_.join()
 
-           #_threads_ = []
-           #for _ipaddress_ in valid_access_ip:
-           #   th = threading.Thread(target=caching_policy, args=(_ipaddress_,ip_device_dict[_ipaddress_],))
-           #   th.start()
-           #   _threads_.append(th)
-           #for th in _threads_:
-           #   th.join()
 
            # return
            return Response(viewer_information())
