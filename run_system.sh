03bb9069f9d9bbfd5261fb09564d4247e50c96c4
Your Name <you@example.com>
2017-03-07 11:27:40 +0900

    update

--- run_system.sh
+++ run_system.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
-PORT=$(cat f5restapi/setting.py | grep -i 'RUNSERVER_PORT' | awk -F"[ ']" '{print $4}')
+SRV_LISTEN_IP='192.168.56.102'
+SRV_LISTEN_PORT='8080'
 
-python manage.py runserver 0.0.0.0:$PORT
+python manage.py runserver $SRV_LISTEN_IP:$SRV_LISTEN_PORT
 
 
 
