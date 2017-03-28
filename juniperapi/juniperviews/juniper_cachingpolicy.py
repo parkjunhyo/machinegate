402cfeeda3e9f49197016c42f70ef9d962abc309
park junhyo <junhyo.luck@gmail.com>
2017-03-29 02:57:19 +0900

    Update juniper_cachingpolicy.py

--- juniperapi/juniperviews/juniper_cachingpolicy.py
+++ juniperapi/juniperviews/juniper_cachingpolicy.py
@@ -230,6 +230,11 @@ def caching_policy(_filename_, this_processor_queue):
                 _src_port_cache_, _rule_cache_[_policy_name_sequence_]['src_port'] = _caching_port_process_(_source_app_pattern_, _app_string_, _app_proto_, _src_port_cache_, _policy_name_sequence_, _rule_cache_[_policy_name_sequence_]['src_port'])
                 _dst_port_cache_, _rule_cache_[_policy_name_sequence_]['dst_port'] = _caching_port_process_(_destination_app_pattern_, _app_string_, _app_proto_, _dst_port_cache_, _policy_name_sequence_, _rule_cache_[_policy_name_sequence_]['dst_port'])
 
+           elif re.search('esp',_app_proto_) or re.search('ah',_app_proto_):
+             for _app_string_ in _application_contents_:
+                _src_port_cache_, _rule_cache_[_policy_name_sequence_]['src_port'] = _caching_port_process_(_source_app_pattern_, _app_string_, _app_proto_, _src_port_cache_, _policy_name_sequence_, _rule_cache_[_policy_name_sequence_]['src_port'])
+                _dst_port_cache_, _rule_cache_[_policy_name_sequence_]['dst_port'] = _caching_port_process_(_destination_app_pattern_, _app_string_, _app_proto_, _dst_port_cache_, _policy_name_sequence_, _rule_cache_[_policy_name_sequence_]['dst_port'])     
+                
            elif re.search('0',_app_proto_):
              for _app_string_ in _application_contents_:
                 _src_port_cache_, _rule_cache_[_policy_name_sequence_]['src_port'] = _caching_port_process_(_source_app_pattern_, _app_string_, 'tcp', _src_port_cache_, _policy_name_sequence_, _rule_cache_[_policy_name_sequence_]['src_port'])
